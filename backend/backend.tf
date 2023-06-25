d20fc0020b07dda82b816b9b08d0fb13e1e15c5c
Adewale Eniola <104868415+3ni0lA@users.noreply.github.com>
2023-06-25 19:20:17 +0100

    Add files via upload

--- backend/backend.tf
+++ backend/backend.tf
@@ -7,130 +7,148 @@ provider "aws" {
  resource "aws_s3_bucket" "statefile-bucket" {
   bucket = "sockapp-bucket"
 
-   tags = {
-    Name        = "Mysockappbucket"
-    Environment = "Dev"
+  
+
+
+  lifecycle {
+    prevent_destroy = true
   }
-}
 
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.statefile-bucket.id
-  acl    = "private"
-}
+  versioning {
+    enabled = true
+  }
 
-resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
-  bucket = aws_s3_bucket.statefile-bucket.id
-
-  rule {
-    id = "log"
-
-    expiration {
-      days = 90
-    }
-
-    filter {
-      and {
-        prefix = "log/"
-
-        tags = {
-          rule      = "log"
-          autoclean = "true"
-        }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
       }
     }
-
-    status = "Enabled"
-
-    transition {
-      days          = 30
-      storage_class = "STANDARD_IA"
-    }
-
-    transition {
-      days          = 60
-      storage_class = "GLACIER"
-    }
   }
+ }
 
-  rule {
-    id = "tmp"
+# # resource "aws_s3_bucket_acl" "bucket_acl" {
+# #   bucket = aws_s3_bucket.statefile-bucket.id
+# #   acl    = "private"
+# # }
 
-    filter {
-      prefix = "tmp/"
-    }
+# resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
+#   bucket = aws_s3_bucket.statefile-bucket.id
 
-    expiration {
-      date = "2023-08-20T01:00:00+01:00"
-    }
+#   rule {
+#     id = "log"
 
-    status = "Enabled"
-  }
-}
+#     expiration {
+#       days = 90
+#     }
 
-resource "aws_s3_bucket" "versioning_bucket" {
-  bucket = "sockapp-version-bucket"
-}
+#     filter {
+#       and {
+#         prefix = "log/"
 
-resource "aws_s3_bucket_acl" "versioning_bucket_acl" {
-  bucket = aws_s3_bucket.versioning_bucket.id
-  acl    = "private"
-}
+#         tags = {
+#           rule      = "log"
+#           autoclean = "true"
+#         }
+#       }
+#     }
 
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.versioning_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#     status = "Enabled"
 
-resource "aws_s3_bucket_lifecycle_configuration" "versioning-bucket-config" {
-  # Must have bucket versioning enabled first
-  depends_on = [aws_s3_bucket_versioning.versioning]
+#     transition {
+#       days          = 30
+#       storage_class = "STANDARD_IA"
+#     }
 
-  bucket = aws_s3_bucket.versioning_bucket.id
+#     transition {
+#       days          = 60
+#       storage_class = "GLACIER"
+#     }
+#   }
 
-  rule {
-    id = "config"
+#   rule {
+#     id = "tmp"
 
-    filter {
-      prefix = "config/"
-    }
+#     filter {
+#       prefix = "tmp/"
+#     }
 
-    noncurrent_version_expiration {
-      noncurrent_days = 90
-    }
+#     expiration {
+#       date = "2023-08-20T01:00:00+01:00"
+#     }
 
-    noncurrent_version_transition {
-      noncurrent_days = 30
-      storage_class   = "STANDARD_IA"
-    }
+#     status = "Enabled"
+#   }
+# }
+# # 
 
-    noncurrent_version_transition {
-      noncurrent_days = 60
-      storage_class   = "GLACIER"
-    }
+# resource "aws_s3_bucket" "versioning_bucket" {
+#   bucket = "sockapp-version-bucket"
+# }
 
-    status = "Enabled"
-  }
-}
-# Provides a S3 bucket server-side encryption configuration resource.
-resource "aws_kms_key" "mykey" {
-  description             = "This key is used to encrypt bucket objects"
-  deletion_window_in_days = 30
-}
+# # resource "aws_s3_bucket_acl" "versioning_bucket_acl" {
+# #   bucket = aws_s3_bucket.versioning_bucket.id
+# #   acl    = "private"
+# # }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "server-config" {
-  bucket = aws_s3_bucket.statefile-bucket.id
+# resource "aws_s3_bucket_versioning" "versioning" {
+#   bucket = aws_s3_bucket.versioning_bucket.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.mykey.arn
-      sse_algorithm     = "aws:kms"
-    }
-  }
-}
+# resource "aws_s3_bucket_lifecycle_configuration" "versioning-bucket-config" {
+#   # Must have bucket versioning enabled first
+#   depends_on = [aws_s3_bucket_versioning.versioning]
 
+#   bucket = aws_s3_bucket.versioning_bucket.id
 
+#   rule {
+#     id = "config"
+
+#     filter {
+#       prefix = "config/"
+#     }
+
+#     noncurrent_version_expiration {
+#       noncurrent_days = 90
+#     }
+
+#     noncurrent_version_transition {
+#       noncurrent_days = 30
+#       storage_class   = "STANDARD_IA"
+#     }
+
+#     noncurrent_version_transition {
+#       noncurrent_days = 60
+#       storage_class   = "GLACIER"
+#     }
+
+#     status = "Enabled"
+#   }
+# }
+# # Provides a S3 bucket server-side encryption configuration resource.
+# resource "aws_kms_key" "mykey" {
+#   description             = "This key is used to encrypt bucket objects"
+#   deletion_window_in_days = 30
+# }
+
+# resource "aws_s3_bucket_server_side_encryption_configuration" "server-config" {
+#   bucket = aws_s3_bucket.statefile-bucket.id
+
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       kms_master_key_id = aws_kms_key.mykey.arn
+#       sse_algorithm     = "aws:kms"
+#     }
+#   }
+# }
+
+#  tags = {
+  #   Name        = "Mysockappbucket"
+  #   Environment = "Dev"
+  # }
 
 # Create Dynamo DB table for backend statefile locking
 
