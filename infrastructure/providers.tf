9f58b9d3efaeb36fef5f7f34d3137a9b77385e54
Adewale Eniola <104868415+3ni0lA@users.noreply.github.com>
2023-07-03 20:48:21 +0100

    Added new lines

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -2,17 +2,15 @@
 
 provider "aws" {
   region     = "eu-west-2"
-  access_key = var.accesskey
-  secret_key = var.secretkey
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 
 }
-  provider "aws" {
-  region  = var.region
-}
-
+ 
 terraform {
   required_providers {
-           kubernetes = {
+  
+        kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.0"
     }
@@ -40,7 +38,7 @@ terraform {
 
 terraform {
   backend "s3" {
-    bucket = "terraform-state-08174509694"
+    bucket = "sockapp-bucket"
     key = "global/infrastructure/terraform.tfstate"
     region     = "eu-west-2"
     dynamodb_table = "terraform-state-lock"
@@ -74,3 +72,14 @@ provider "kubectl" {
   }
 }
  
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.eks-cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks-cluster.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks-cluster.name]
+      command     = "aws"
+    }
+  }
+}
