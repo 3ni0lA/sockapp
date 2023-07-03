3bdb72483564d693f4e216528a66a26b1520c9b6
Adewale Eniola <104868415+3ni0lA@users.noreply.github.com>
2023-07-03 20:46:00 +0100

    Added new lines

--- infrastructure/eks-cluster.tf
+++ infrastructure/eks-cluster.tf
@@ -48,19 +48,6 @@ resource "aws_eks_node_group" "eks-node-group" {
 
 
 }
-module "ebs_csi_eks_role" {
-  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  role_name = "ebs_csi"
-
-  attach_ebs_csi_policy = true
-
-  oidc_providers = {
-    main = {
-      provider_arn               = local.oidc_provider_name
-      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
-    }
-  }
-}
 
 
 ###################
@@ -90,42 +77,10 @@ resource "helm_release" "ebs_csi_driver" {
   
 }
 
-# resource "helm_release" "ebs_csi_driver" {
-#   name       = "aws-ebs-csi-driver"
-#   namespace  = "kube-system"
-#   repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
-#   chart      = "aws-ebs-csi-driver"
-
-#   set {
-#     name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-#     type  = "string"
-#     value = module.ebs_csi_eks_role.iam_role_arn
-#   }
-# }
-
-
 ###################
 # Storage Classes #
 ###################
 
-# resource "kubernetes_storage_class_v1" "storageclass_gp2" {
-#   depends_on = [helm_release.ebs_csi_driver, module.ebs_csi_eks_role]
-#   metadata {
-#     name = "gp2-encrypted"
-#     annotations = {
-#       "storageclass.kubernetes.io/is-default-class" = "true"
-#     }
-#   }
-
-#   storage_provisioner    = "ebs.csi.aws.com"
-#   reclaim_policy         = "Delete"
-#   allow_volume_expansion = true
-#   volume_binding_mode    = "WaitForFirstConsumer"
-
-#   parameters = {
-#     type      = "gp2"
-#     encrypted = "true"
-#   }
 resource "kubernetes_storage_class" "ebs_sc" {
   metadata {
     name = "ebs-sc"
